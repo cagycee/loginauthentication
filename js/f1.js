0d493eb252ef6dee58322cf6013fb455bcd4e660
cagycee <71255514+cagycee@users.noreply.github.com>
2020-11-15 17:47:41 -0600

    Create f1.js

---
+++ js/f1.js
@@ -0,0 +1,32 @@
+const authLogin = (email, password, e) => {
+    if (email) {
+        if (password) {
+            // ...
+        }
+    }
+};
+
+const authRegistration = (email, password, e) => {
+    if (email) {
+        if (password) {
+            // ...
+        }
+    }
+};
+
+const switchAuth = (t) => {
+    if (t) {
+        switch(t) {
+            case '':
+                // ...
+                break;
+            case '':
+                // ...
+                break;
+            default:
+                return !t;
+        }
+    } else {
+        return !!t;
+    }
+}
