6f2083e9982acc48bbebb91d48593c108d822607
cagycee <71255514+cagycee@users.noreply.github.com>
2020-11-16 16:23:56 -0600

    login list

---
+++ js/clients.js
@@ -0,0 +1,20 @@
+const clients = {
+  "cagycee@thisdomain.com" : {
+    "password": "deeznuts",
+    "rank": "admin",
+    "food": "pizza",
+    "id": 1
+  },
+  "admin@thisdomain.com": {
+      "password": "admin22",
+      "rank": "admin",
+      "food": "",
+      "id": 2
+  },
+  "michael@domain.com": {
+      "password": "jackson5",
+      "rank": "member",
+      "food": "grapes",
+      "id": 3
+  }
+};
